efe34a1903c93014f0098b2d2cd38aa0e45cbc95
zhang96 <zhang96@users.noreply.github.com>
2016-03-05 15:11:47 -0500

    Weather displayed properly not.

--- weatherboard.ino
+++ weatherboard.ino
@@ -1,60 +1,28 @@
-/*
-  The circuit:
- * LCD RS pin to digital pin 12
- * LCD Enable pin to digital pin 11
- * LCD D4 pin to digital pin 5
- * LCD D5 pin to digital pin 4
- * LCD D6 pin to digital pin 3
- * LCD D7 pin to digital pin 2
- * LCD R/W pin to ground
- * LCD VSS pin to ground
- * LCD VCC pin to 5V
- * 10K resistor:
- * ends to +5V and ground
- * wiper to LCD VO pin (pin 3)
- */
-
-// include the library code:
 #include <LiquidCrystal.h>
 #include <dht.h>
-#define dht_dpin A0 //no ; here. Set equal to channel sensor is on
-
-dht DHT;
+#include <string.h>
 
 // initialize the library with the numbers of the interface pins
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+dht DHT;
+double humidity;
+double temp;
 
 void setup() {
-  // set up the LCD's number of columns and rows:
   lcd.begin(16, 2);
-  // Print a message to the LCD.
-  lcd.print("hello, world!");
-  
   Serial.begin(9600);
-  delay(300);//Let system settle
-//  Serial.println("Humidity and temperature\n\n");
-  lcd.print("Humidity and temperature");
-  delay(700);//Wait rest of 1000ms recommended delay before
-  //accessing sensor
+  delay(1000);
  
 }
 
 void loop() {
- 
-    // set the cursor to column 0, line 1
-    // (note: line 1 is the second row, since counting begins with 0):
-    lcd.setCursor(0, 1);
+    DHT.read11(A0);
+    humidity = DHT.humidity;
+    temp = DHT.temperature;
+    lcd.print(humidity);
+	  lcd.setCursor(0, 1);
+	  lcd.print(temp);
+    delay(1000);
+    lcd.clear();
+} 
 
-    DHT.read11(dht_dpin);
-    Serial.print("Current humidity = ");
-    Serial.print(DHT.humidity);
-    lcd.print(DHT.humidity);
-    Serial.print("%  ");
-    Serial.print("temperature = ");
-    Serial.print(DHT.temperature); 
-    Serial.println("C  ");
-    delay(800);//Don't try to access too frequently... in theory
-  //should be once per two seconds, fastest,
-  //but seems to work after 0.8 second.
-  
-}
