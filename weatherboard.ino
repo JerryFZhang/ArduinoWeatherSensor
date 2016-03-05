4e87e2fea6e58385dec6b6bda15fa00ec5ad4748
zhang96 <zhang96@users.noreply.github.com>
2016-03-05 11:07:32 -0500

    Zip has been added.

--- weatherboard.ino
+++ weatherboard.ino
@@ -17,7 +17,6 @@
 // include the library code:
 #include <LiquidCrystal.h>
 #include <dht.h>
-
 #define dht_dpin A0 //no ; here. Set equal to channel sensor is on
 
 dht DHT;
@@ -26,34 +25,36 @@ dht DHT;
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
 void setup() {
-    Serial.begin(9600);
-  delay(300);//Let system settle
-  Serial.println("Humidity and temperature\n\n");
-  delay(700);//Wait rest of 1000ms recommended delay before
-  //accessing sensor
   // set up the LCD's number of columns and rows:
   lcd.begin(16, 2);
   // Print a message to the LCD.
   lcd.print("hello, world!");
+  
+  Serial.begin(9600);
+  delay(300);//Let system settle
+//  Serial.println("Humidity and temperature\n\n");
+  lcd.print("Humidity and temperature");
+  delay(700);//Wait rest of 1000ms recommended delay before
+  //accessing sensor
+ 
 }
 
 void loop() {
-  //This is the "heart" of the program.
-  DHT.read11(dht_dpin);
+ 
+    // set the cursor to column 0, line 1
+    // (note: line 1 is the second row, since counting begins with 0):
+    lcd.setCursor(0, 1);
 
+    DHT.read11(dht_dpin);
     Serial.print("Current humidity = ");
     Serial.print(DHT.humidity);
+    lcd.print(DHT.humidity);
     Serial.print("%  ");
     Serial.print("temperature = ");
     Serial.print(DHT.temperature); 
     Serial.println("C  ");
-  delay(800);//Don't try to access too frequently... in theory
+    delay(800);//Don't try to access too frequently... in theory
   //should be once per two seconds, fastest,
   //but seems to work after 0.8 second.
   
-  // set the cursor to column 0, line 1
-  // (note: line 1 is the second row, since counting begins with 0):
-  lcd.setCursor(0, 1);
-  // print the number of seconds since reset:
-  lcd.print(millis() / 1000);
 }
