29e80adbd31e85a454c545e7bf623952d29123e1
zhang96 <zhang96@users.noreply.github.com>
2016-03-06 10:22:13 -0500

    Inline comment added.

--- weatherboard.ino
+++ weatherboard.ino
@@ -10,25 +10,38 @@ double temp;
 void setup() {
 	// Initialize the lines and columns of display will be used.
   	lcd.begin(16, 2);
-	lcd.print("    Welcome!");
+	
+	lcd.print("    Welcome!"); // Extra spacing added for alignment purpose.
   	
-	// Wait for one 
+	// Wait for one second
 	delay(1000);
  	
+	//Clear the display for displaying the data.
 	lcd.clear();
 }
 
 void loop() {
+	// Fetching data from the sensor.
     DHT.read11(A0);
+	
+	// Store the weather information in a global variable.
     humidity = DHT.humidity;
     temp = DHT.temperature;
-    lcd.print("TEMP:");
-	  lcd.print(temp);
+    
+	//Print the Temperature information on the display.
+	lcd.print("TEMP:");
+	lcd.print(temp);
     lcd.print("C");
+	
+	//Set the cursor so the print start from the index 1, which is the second line.
     lcd.setCursor(0, 1);
-    lcd.print("HUMI:");
+    
+	// Print the humidity information on the display.
+	lcd.print("HUMI:");
     lcd.print(humidity);
     lcd.print("%");
+	
+	// Wait for a second.
     delay(1000);
     lcd.clear();
 } 
